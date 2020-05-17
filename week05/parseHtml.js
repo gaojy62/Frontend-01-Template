a51f58dae9a7ee2f13cd008021e9ff2f5636a9b9
gao <wygaojy@163.com>
2020-05-17 21:13:58 +0800

    cdd dom

--- week05/parseHtml.js
+++ week05/parseHtml.js
@@ -1,9 +1,59 @@
 const EOF = Symbol('EOF')
+
 let currentToken = null
 let currentAttribute = null
+let currentTextNode = null
+
+let stack = [{ type: 'document', children: [] }]
 
 function emit(token) {
-  console.log(token)
+  let top = stack[stack.length - 1]
+
+  if (token.type === 'startTag') {
+    let element = {
+      type: 'element',
+      children: [],
+      attributes: [],
+      tagName: token.tagName,
+    }
+
+    for (let p in token) {
+      if (p !== 'type' && p !== 'tagName') {
+        element.attributes.push({
+          name: p,
+          value: token[p],
+        })
+      }
+    }
+
+    top.children.push(element)
+    element.parent = top
+
+    // 自封闭标签不需要入栈
+    if (!token.isSelfClosing) {
+      stack.push(element)
+    }
+    currentTextNode = null
+  } else if (token.type === 'endTag') {
+    if (top.tagName !== token.tagName) {
+      throw new Error('tag name do not match')
+    } else {
+      // 非自封闭标签匹配完成后出栈
+      stack.pop()
+    }
+    // top.children.push(currentTextNode)
+    currentTextNode = null
+  } else if (token.type === 'text') {
+    // 处理文本节点，文本节点直接追加到父节点的子元素上
+    if (currentTextNode === null) {
+      currentTextNode = {
+        type: 'text',
+        content: '',
+      }
+      top.children.push(currentTextNode)
+    }
+    currentTextNode.content += token.content
+  }
 }
 
 function data(c) {
@@ -12,13 +62,12 @@ function data(c) {
   } else if (c === EOF) {
     emit({
       type: 'EOF',
-      tagName: null,
     })
     return
   } else {
     emit({
       type: 'text',
-      tagName: c,
+      content: c,
     })
     return data
   }
@@ -33,22 +82,27 @@ function tagOpen(c) {
       tagName: '',
     }
     return tagName(c)
+  } else if (c === EOF) {
+    throw new Error('eof-before-tag-name parse-error')
   } else {
-    return tagOpen
+    console.log('invalid-first-character-of-tag-name parse error.')
+    return data(c)
   }
 }
 
 function tagName(c) {
   if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName
-  } else if (c.match(/^[a-zA-Z]$/)) {
-    currentToken.tagName += c.toLowerCase()
-    return tagName
   } else if (c === '/') {
     return selfClosingStartTag
   } else if (c === '>') {
     emit(currentToken)
     return data
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken.tagName += c.toLowerCase()
+    return tagName
+  } else if (c === EOF || c === '\u0000') {
+    throw new Error('parse-error')
   } else {
     currentToken.tagName += c
     return tagName
@@ -62,6 +116,12 @@ function beforeAttributeName(c) {
     return afterAttributeName(c)
   } else if (c === '=') {
     // 抛错
+    console.log('unexpected-equals-sign-before-attribute-name parse error')
+    currentAttribute = {
+      name: '',
+      value: '',
+    }
+    return attributeName
   } else {
     currentAttribute = {
       name: '',
@@ -71,8 +131,28 @@ function beforeAttributeName(c) {
   }
 }
 
+function attributeName(c) {
+  if (c.match(/^[\n\f\t ]$/) || c === '/' || c === EOF || c === '>') {
+    return afterAttributeName(c)
+  } else if (c === '=') {
+    return beforeAttributeValue
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    currentAttribute.name += c
+    return attributeName
+  } else if (c === '\u0000') {
+    throw new Error('unexpected-null-character parse error.')
+  } else if (c === '"' || c === "'" || c === '<') {
+    throw new Error('unexpected-character-in-attribute-name parse error.')
+  } else {
+    currentAttribute.name += c
+    return attributeName
+  }
+}
+
 function afterAttributeName(c) {
-  if (c === '/') {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return afterAttributeName
+  } else if (c === '/') {
     return selfClosingStartTag
   } else if (c === '=') {
     return beforeAttributeValue
@@ -80,19 +160,14 @@ function afterAttributeName(c) {
     emit(currentToken)
     return data
   } else if (c === EOF) {
+    throw new Error('eof-in-tag parse error.')
   } else {
+    // 有歧义
+    currentAttribute = {
+      name: '',
+      value: '',
+    }
     return attributeName(c)
-  }
-}
-
-function attributeName(c) {
-  if (c.match(/^[\n\f\t ]$/) || c === '/' || c === EOF || c === '>') {
-    return afterAttributeName(c)
-  } else if (c === '=') {
-    return beforeAttributeValue
-  } else {
-    currentAttribute.name += c
-    return attributeName
   }
 }
 
@@ -105,6 +180,7 @@ function beforeAttributeValue(c) {
     return doubleQuotedAttributeValue
   } else if (c === '>') {
     //抛错
+    throw new Error(' missing-attribute-value parse error.')
   } else {
     return unquotedAttributeValue(c)
   }
@@ -112,10 +188,13 @@ function beforeAttributeValue(c) {
 
 function doubleQuotedAttributeValue(c) {
   if (c === '"') {
+    // 有歧义
     currentToken[currentAttribute.name] = currentAttribute.value
     return afterQuotedAttributeValue
   } else if (c === '\u0000') {
+    throw new Error('unexpected-null-character parse error.')
   } else if (c === EOF) {
+    throw new Error('eof-in-tag parse error.')
   } else {
     currentAttribute.value += c
     return doubleQuotedAttributeValue
@@ -127,7 +206,9 @@ function singleQuotedAttributeValue(c) {
     currentToken[currentAttribute.name] = currentAttribute.value
     return afterQuotedAttributeValue
   } else if (c === '\u0000') {
+    throw new Error('unexpected-null-character parse error.')
   } else if (c === EOF) {
+    throw new Error('eof-in-tag parse error.')
   } else {
     currentAttribute.value += c
     return doubleQuotedAttributeValue
@@ -140,26 +221,34 @@ function afterQuotedAttributeValue(c) {
   } else if (c === '/') {
     return selfClosingStartTag
   } else if (c === '>') {
+    // 有歧义
     currentToken[currentAttribute.name] = currentAttribute.value
     emit(currentAttribute)
     return data
+  } else if (c === EOF) {
+    throw new Error('eof-in-tag parse error.')
+  } else {
+    console.log('missing-whitespace-between-attributes parse error.')
+    return beforeAttributeName(c)
   }
 }
 
 function unquotedAttributeValue(c) {
-  if (c.match(/^[\t\n\f]$/)) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    // 有歧义
     currentToken[currentAttribute.name] = currentAttribute.value
     return beforeAttributeName
-  } else if (c === '/') {
-    currentToken[currentAttribute.name] = currentAttribute.value
-    return selfClosingStartTag
-  } else if (c === '=') {
-    currentToken[currentAttribute.name] = currentAttribute.value
+  } else if (c === '>') {
     emit(currentToken)
     return data
   } else if (c === '\u0000') {
+    throw new Error('unexpected-null-character parse error. ')
   } else if (c === EOF) {
-  } else if (c === '"' || c === "'" || c === '<' || c === '=') {
+    throw new Error('eof-in-tag parse error.')
+  } else if (c === '"' || c === "'" || c === '<' || c === '=' || c === '`') {
+    throw new Error(
+      ' unexpected-character-in-unquoted-attribute-value parse error.'
+    )
   } else {
     currentAttribute.value += c
     return unquotedAttributeValue
@@ -167,11 +256,15 @@ function unquotedAttributeValue(c) {
 }
 
 function selfClosingStartTag(c) {
-  if (c.match(/^[\t\n\f ]$/)) {
-    return selfClosingStartTag
-  } else if (c === '>') {
+  if (c === '>') {
     currentToken.isSelfClosing = true
+    emit(currentToken)
     return data
+  } else if (c === EOF) {
+    throw new Error('eof-in-tag parse error.')
+  } else {
+    console.log('unexpected-solidus-in-tag parse error')
+    return beforeAttributeName(c)
   }
 }
 
@@ -183,8 +276,11 @@ function endTagOpen(c) {
     }
     return tagName(c)
   } else if (c === '>') {
+    throw new Error('missing-end-tag-name parse error.')
   } else if (c === EOF) {
+    throw new Error('eof-before-tag-name parse error.')
   } else {
+    throw new Error(' invalid-first-character-of-tag-name parse error.')
   }
 }
 
@@ -194,4 +290,5 @@ module.exports.parseHtml = function parseHtml(html) {
     state = state(c)
   }
   state = state(EOF)
+  console.log(stack[0].children[0].children[1].children[1])
 }
