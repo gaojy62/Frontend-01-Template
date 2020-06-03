0a906c034a51f26952ba3d3c6686b5a08f2c51e1
gao <wygaojy@163.com>
2020-06-03 23:22:33 +0800

    第八周作业

--- week08/NOTE.md
+++ week08/NOTE.md
@@ -1,1 +1,59 @@
-# 每周总结可以写在这里
+# CSS
+
+## css选择器分类
+
+- 简单选择器
+  - id
+  - class
+  - tagName
+  - attribute
+    - [att] 只要元素有这个属性，就可以被选中
+    - [att=val] 精确匹配，检查一个 att 属性的值是否是 val
+    - [attr~=value] 表示带有以 attr 命名的属性的元素，并且该属性是一个以空格作为分隔的值列表，其中至少有一个值为 value
+    - [attr|=value] 表示带有以 attr 命名的属性的元素，属性值为“value”或是以“value-”为前缀（"-"为连字符，Unicode 编码为 U+002D）开头。典型的应用场景是用来匹配语言简写代码（如 zh-CN，zh-TW 可以用 zh 作为 value）
+    - [attr^=value] 带有以 attr 命名的属性，且属性值是以 value 开头的元素
+    - [attr$=value] 表示带有以 attr 命名的属性，且属性值是以 value 结尾的元素
+    - [attr*=value] 表示带有以 attr 命名的属性，且属性值包含有 value 的元素
+  - 伪元素选择器
+    - ::before
+    - ::after
+    - first-line
+      - font 系列
+      - color 系列
+      - background 系列
+      - word-spacing
+      - letter-spacing
+      - text-decoration
+      - text-transform
+      - line-height
+    - first-letter
+      - font 系列
+      - color 系列
+      - background 系列
+      - word-spacing
+      - letter-spacing
+      - text-decoration
+      - text-transform
+      - line-height
+      - float
+      - vertical-align
+      - 盒模型系列：margin、padding、border 
+- 复合选择器
+- 复杂选择器
+  - \>
+  - ~
+  - \+
+  - 空格
+
+## 优先级
+
+`!important` > `inline` > `id > class | 伪类 | attribute` > `tagName | 伪元素` > `* | 关系选择器 | 否定类选择器  (这三个不影响优先级)`
+
+## BFC
+
+如果一个元素具有 BFC，那么它不会影响外部的元素。所以，BFC 元素是不可能发生 margin 重叠的，因为 margin 重叠是会影响外部的元素的。同时 BFC 可以用来清楚浮动带来的高度塌陷问题
+
+- block-level 表示可以被放入 bfc
+- block-container 表示可以容纳 bfc
+- block-box = block-level + block-container
+- block-box 如果 overflow 是 visible， 那么就跟父 bfc 合并
--- week08/match.js
+++ week08/match.js
@@ -1,8 +1,150 @@
-function mathc (selector, dom) {
-  // > 直接子节点，  ' ' 子孙组合器， ~ 一般兄弟组合器， + 紧邻组合器
-  // : 伪类 ， ::伪元素
-  // id class attribute name
-  // *
+// > 直接子节点，  ' ' 子孙组合器， ~ 一般兄弟组合器， + 紧邻组合器
+// : 伪类 ， ::伪元素
+// id class attribute name
+// *
 
-  
+let currentClass = ''
+let currentId = ''
+let tagName = ''
+let tagId = []
+let tagClass = []
+
+function compositeSelectors(string) {
+  tagId = []
+  tagClass = []
+  tagName = ''
+  let state = start
+  for (let c of string) {
+    state = state(c)
+  }
+  state('\u0000')
+  return {
+    tagName,
+    tagId,
+    tagClass,
+  }
+}
+
+function start(c) {
+  if (c === '\u0000') {
+    return
+  } else if (c.match(/[a-zA-Z]/)) {
+    return setTagName(c)
+  } else if (c === '.') {
+    return setClass
+  } else if (c === '#') {
+    return setId
+  }
+}
+
+function setTagName(c) {
+  if (c === '\u0000') {
+    return
+  } else if (c === '.') {
+    return setClass
+  } else if (c === '#') {
+    return setId
+  } else {
+    tagName += c
+    return setTagName
+  }
+}
+
+function setClass(c) {
+  if (c === '\u0000') {
+    tagClass.push(currentClass)
+    currentClass = ''
+  } else if (c === '.') {
+    tagClass.push(currentClass)
+    currentClass = ''
+    return setClass
+  } else if (c === '#') {
+    tagClass.push(currentClass)
+    currentClass = ''
+    return setId
+  } else {
+    currentClass += c
+    return setClass
+  }
+}
+
+function setId(c) {
+  if (c === '\u0000') {
+    tagId.push(currentId)
+    currentId = ''
+  } else if (c === '.') {
+    tagId.push(currentId)
+    currentId = ''
+    return setClass
+  } else if (c === '#') {
+    tagId.push(currentId)
+    currentId = ''
+    return setId
+  } else {
+    currentId += c
+    return setId
+  }
+}
+
+// 只是实现了复合选择器（不包括属性），没有实现复杂选择器
+function match(element, selector) {
+  let matchId = (matchClass = matchTagName = false)
+  if (!selector || !element.attributes) return false
+
+  let compositeSelector = compositeSelectors(selector)
+
+  if (compositeSelector.tagId.length > 0) {
+    let attr = element.attributes.filter((attr) => attr.name === 'id')
+    if (attr.length > 0) {
+      if (isContain(attr[0].value.split(' '), compositeSelector.tagId)) {
+        matchId = true
+      }
+    }
+  } else {
+    matchId = true
+  }
+
+  if (compositeSelector.tagClass.length > 0) {
+    let attr = element.attributes.filter((attr) => attr.name === 'class')
+    if (attr.length > 0) {
+      if (isContain(attr[0].value.split(' '), compositeSelector.tagClass)) {
+        matchClass = true
+      }
+    }
+  } else {
+    matchId = true
+  }
+
+  if (compositeSelector.tagName.length > 0) {
+    if (element.tagName === compositeSelector.tagName) {
+      matchTagName = true
+    }
+  } else {
+    matchTagName = true
+  }
+
+  if (matchId && matchClass && matchTagName) return true
+
+  return false
+}
+
+function isContain(arr1, arr2) {
+  for (let item of arr2) {
+    // 每开始找一个值就把标志位置为 false，如果后续找到就置为 true，
+    // 当内层循环完成后值还为 false 则返回 false 表示没有匹配成功
+    // 如果外层循环完没有返回 false 则表示匹配成功，返回 true
+    let itemInArr1 = false
+    for (let itemOfArr1 of arr1) {
+      // 如果有对应值则跳出循环，继续寻找 arr2 中的下一个值
+      if (itemOfArr1 === item) {
+        itemInArr1 = true
+        continue
+      }
+    }
+    // 如果当前值 item 在 arr1 中没有对应值，则说明不匹配
+    if (!itemInArr1) {
+      return false
+    }
+  }
+  return true
 }
